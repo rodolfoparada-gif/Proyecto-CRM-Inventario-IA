3123d517bfd48b7522c005857cd128961a0b8e72
Rodolfo Parada <rodolfo.parada@sellside.cl>
2026-02-01 20:12:39 -0300

    feat: modificaciòn manifest y chat_loader.js

--- addons/crm_inventory_ai/__manifest__.py
+++ addons/crm_inventory_ai/__manifest__.py
@@ -1,28 +1,29 @@
 {
     'name': 'Agente IA-CRM-Inventario',
-    'version': '19.0.1.0',
+    'version': '1.0',
     'category': 'Productivity',
     'summary': 'Asistente IA para Inventario y CRM en todo Odoo',
-    'author': 'Mi señor',
+    'author': 'Rodolfo Parada',
     'depends': ['base', 'web', 'stock', 'crm', 'website'],
     'data': [
         'security/ir.model.access.csv',
         'data/ir_config_parameter.xml',
         'views/res_config_settings_views.xml',
-],
-  'assets': {
-    'web.assets_backend': [
-        'Proyecto-CRM-Inventario-IA/static/src/components/chat_bubble/chat_bubble.xml',
-        'Proyecto-CRM-Inventario-IA/static/src/components/chat_bubble/chat_bubble.js',
-        'Proyecto-CRM-Inventario-IA/static/src/components/chat_bubble/chat_bubble.scss',
-        'Proyecto-CRM-Inventario-IA/static/src/js/chat_loader.js',
     ],
-    'website.assets_frontend': [
-        'Proyecto-CRM-Inventario-IA/static/src/components/chat_bubble/chat_bubble.js',
-        'Proyecto-CRM-Inventario-IA/static/src/components/chat_bubble/chat_bubble.xml',
-        'Proyecto-CRM-Inventario-IA/static/src/components/chat_bubble/chat_bubble.scss',
-    ],
-},
+    'assets': {
+        'web.assets_backend': [
+            'Proyecto-CRM-Inventario-IA/static/src/components/**/*.js',
+            'Proyecto-CRM-Inventario-IA/static/src/components/**/*.xml',
+            'Proyecto-CRM-Inventario-IA/static/src/components/**/*.scss',
+            'Proyecto-CRM-Inventario-IA/static/src/js/chat_loader.js',
+        ],
+        'website.assets_frontend': [
+            'Proyecto-CRM-Inventario-IA/static/src/components/**/*.js',
+            'Proyecto-CRM-Inventario-IA/static/src/components/**/*.xml',
+            'Proyecto-CRM-Inventario-IA/static/src/components/**/*.scss',
+            'Proyecto-CRM-Inventario-IA/static/src/js/chat_loader.js',
+        ],
+    },
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
--- addons/crm_inventory_ai/static/src/js/chat_loader.js
+++ addons/crm_inventory_ai/static/src/js/chat_loader.js
@@ -1,8 +1,7 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
-import { ChatBubble } from "@agente_ia/components/chat_bubble/chat_bubble";
+import { ChatBubble } from "@Proyecto-CRM-Inventario-IA/components/chat_bubble/chat_bubble";
 
-// Usamos el alias del módulo definido en el manifest
 registry.category("main_components").add("AIChatBubble", {
     Component: ChatBubble,
-});
+}); 

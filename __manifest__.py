33217bd038b71ca31fdb654dc5c6459f3245f801
Rodolfo Parada <rodolfo.parada@sellside.cl>
2026-02-02 01:11:56 -0300

    feat: modificación 20 del manifest

--- __manifest__.py
+++ __manifest__.py
@@ -16,9 +16,7 @@
     "assets": {
         "web.assets_backend": [
             "crm_inventory_ai/static/src/js/chat_loader.js",
-            "crm_inventory_ai/static/src/components/chat/chat.js",
-            "crm_inventory_ai/static/src/components/chat/chat.xml",
-            "crm_inventory_ai/static/src/components/chat/chat.scss",
+          
         ],
     },
     "installable": True,
